2a4cde584ad8c2e6cb2ced2ec9c27b05cef342d1
Frédéric G <giustinifred@gmail.com>
2022-03-04 14:56:12 +0100

    Step 4 Update
    + Add appendChild Version
    - cours.js
    - scripttest.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,44 +1,103 @@
-/** Fetch asynchronous automatic function
+fetch("http://localhost:3000/api/products")
+  .then((response) => response.json())
+  .then((data) => apiData(data));
+
+/** apiData Function description
+ * With the data fetch from the api, the function creates article for each element of the array.
+ */
+function apiData(data) {
+  console.table(data);
+
+  /** forEach description
+   * Execute the function on each keys defined in product
+   */
+  data.forEach((product) => {
+    const { _id, name, imageUrl, description, altTxt } = product;
+
+    const article = document.createElement("article");
+    const link = createLink(_id);
+    const title = createTitle(name);
+    const img = createImage(imageUrl, altTxt);
+    const desc = createDesc(description);
+
+    appendArticleTolink(link, article);
+    appendElementsToArticle(article, img, title, desc);
+  });
+}
+
+// Function: link element creationS
+function createLink(id) {
+  const productLink = document.createElement("a");
+  productLink.href = "./product.html?id=" + id;
+  return productLink;
+}
+
+// Function: img element creation
+function createImage(imageUrl, altTxt) {
+  const productImage = document.createElement("img");
+  productImage.src = imageUrl;
+  productImage.alt = altTxt;
+  return productImage;
+}
+
+// Function: title element creation
+function createTitle(name) {
+  const productTitle = document.createElement("h3");
+  productTitle.textContent = name;
+  productTitle.classList.add("productName");
+  return productTitle;
+}
+
+// Function: description element creation
+function createDesc(description) {
+  const productDesc = document.createElement("p");
+  productDesc.textContent = description;
+  productDesc.classList.add("productDescription");
+  return productDesc;
+}
+
+// Function: Appendchild article to link
+function appendArticleTolink(link, article) {
+  const items = document.querySelector("#items");
+  items.appendChild(link);
+  link.appendChild(article);
+}
+
+// Function: Appendchild elements (img, title and description) to article
+function appendElementsToArticle(article, img, title, desc) {
+  article.appendChild(img);
+  article.appendChild(title);
+  article.appendChild(desc);
+}
+
+/****************************************************************************************************** */
+
+/** SHORT VERSION w innerHTML
+ * Fetch asynchronous automatic function
  * Console log: Sucess or Error
  * Console log: localhost default port 3000
  * Console log: API data
  */
 
+/*
 (async function fetchApiData() {
   const res = await fetch("http://localhost:3000/api/products");
-
-  // Stores API data
   const apiData = await res.json();
-
   if (res.ok) {
-    // Console log Success
     console.log(
       "([ ----- SUCCESS ----- ]): API data was successfully retrieved "
     );
-
-    // Console log localhost default port
     console.log(res);
-
-    // Console log Api datas
     console.log("([ ----- API DATA ARRAY ----- ]):");
     console.table(apiData);
   } else {
-    // Console log Error
     console.log("([ ----- ERROR ----- ]): unable to fetch data from API");
   }
 
-  /** Map and data loading loop
-   * Load for each items from API:
-   * - _id
-   * - imgUrl
-   * - alt
-   * - name
-   * - description
-   */
   document.querySelector("#items").innerHTML = apiData
     .map(
       (item) =>
-        `<a href="./product.html?id=42" id=${item._id}>
+        `<a href="./product.html?${item._id}>
       <article>
         <img src="${item.imageUrl}" alt="${item.altTxt} width=160px height=160px">
         <h3 class="productName">${item.name}</h3>
@@ -46,6 +105,6 @@
       </article>
     </a>`
     )
-    // Delete "," between cards
     .join("");
 })();
+*/
